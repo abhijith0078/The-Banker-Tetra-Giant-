f7ed6a60279701c6d90934c5e06852c73508e0c7
abhijith0078 <aabhijith166@gmail.com>
2022-08-19 23:47:27 +0530

    03 version of banker application. Users,login  added

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 "use strict";
 
+const balanceEl = document.querySelector(".balance-amount");
+const inEl = document.querySelector(".historyIn");
+const outEl = document.querySelector(".historyOut");
+const interestEl = document.querySelector(".interest");
+const welcomeUserEl = document.querySelector(".welcome--user");
+const usernameEl = document.getElementById("username");
+const passwordEl = document.getElementById("password");
+const loginBtnEl = document.getElementById("login--btn");
+const introEl = document.querySelector(".intro");
+const operationsEl = document.querySelector(".operations");
+const historyEl = document.querySelector(".history");
+
 const accounts = [
   {
     fullName: "Abhijith Alakkadan",
-    userName: "Abhijith",
-    pin: 11111,
+    userName: "a",
+    pin: 1,
     movements: [100, -20, 2500, -500, 600, -650, -30, 200, 500, -1000],
     interest: 5.4,
   },
   {
     fullName: "Adarsh Koroth Veetil",
-    userName: "Adarsh",
+    userName: "adarsh",
     pin: 22222,
     movements: [500, 600, -650, 200, 3500, -250, -320, -400, 300],
     interest: 4.5,
   },
   {
     fullName: "Vyshnav Periya Parambath",
-    userName: "Vyshnav",
+    userName: "vyshnav",
     pin: 33333,
     movements: [3500, -400, 300, -250, -320, 250, 340, 980, -750, -450, -200],
     interest: 6.4,
   },
   {
     fullName: "Nived Naadan",
-    userName: "Nived",
+    userName: "nived",
     pin: 44444,
     movements: [
       5000, -250, -300, 520, 5820, -2000, -350, 2540, -950, -320, -400,
@@ -33,10 +45,10 @@ const accounts = [
   },
 ];
 
-const currentUser = accounts[2];
+let currentUser;
 
-function AddTransactions(curUser) {
-  const movements = curUser.movements;
+function AddTransactions(curUsr) {
+  const movements = curUsr.movements;
   movements.forEach(function (element, index, arr) {
     const type = element > 0 ? "deposit" : "withdraw";
     const transactionList = `<li class="transaction">
@@ -55,4 +67,46 @@ function AddTransactions(curUser) {
     transactionUlEl.insertAdjacentHTML("afterbegin", transactionList);
   });
 }
-AddTransactions(currentUser);
+
+const sumArray = function (arr) {
+  const balanceAmount = arr.reduce((acc, ele) => acc + ele, 0);
+  return balanceAmount;
+};
+
+const addBalances = function (curUsr) {
+  const movements = curUsr.movements;
+  const deposit = movements.filter((ele) => ele > 0);
+  const withdraw = movements.filter((ele) => ele < 0);
+
+  balanceEl.textContent = sumArray(movements);
+  inEl.textContent = sumArray(deposit);
+  outEl.textContent = sumArray(withdraw);
+  interestEl.textContent = curUsr.interest;
+};
+
+loginBtnEl.addEventListener("click", function (e) {
+  introEl.classList.add("invisible");
+  operationsEl.classList.add("invisible");
+  historyEl.classList.add("invisible");
+  e.preventDefault();
+
+  const userName = usernameEl.value;
+  const password = Number(passwordEl.value);
+
+  currentUser = accounts.find(function (ele, i, arr) {
+    return ele.userName === userName && ele.pin === password;
+  });
+  console.log(currentUser);
+
+  if (currentUser) {
+    welcomeUserEl.textContent = currentUser.fullName.split(" ")[0];
+    AddTransactions(currentUser);
+    addBalances(currentUser);
+    introEl.classList.remove("invisible");
+    operationsEl.classList.remove("invisible");
+    historyEl.classList.remove("invisible");
+  }
+  usernameEl.value = "";
+  passwordEl.value = "";
+  passwordEl.blur();
+});
